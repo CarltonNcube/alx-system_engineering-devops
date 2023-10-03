cec082793b7f48162869e9c0b142061f7a7e9e49
CarltonNcube <carltonncube@outlook.com>
2023-10-03 13:40:23 -0700

    a Ruby script that accepts one argument and pass it to a regular expression matching method

--- 0x06-regular_expressions/0-simply_match_school.rb
+++ 0x06-regular_expressions/0-simply_match_school.rb
@@ -1,10 +1,2 @@
 #!/usr/bin/env ruby
-def match_school(str)
-  if str.match(/School/)
-    puts "Match found!"
-  else
-    puts "Match not found."
-  end
-end
-
-match_school(ARGV[0])
+puts ARGV[0].scan(/School/).join
