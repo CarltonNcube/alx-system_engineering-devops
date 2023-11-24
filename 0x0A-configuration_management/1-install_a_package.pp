15650b58436b906cee94764d83f830395fa2e616
CarltonNcube <carltonncube@outlook.com>
2023-11-24 11:36:56 -0800

    installing flask from pip3

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -5,5 +5,5 @@ package { 'flask':
   # Ensure the package is installed with the specified version
   ensure   => '2.1.0',
   # Use pip3 as the provider to install the package
-  provider => 'pip3',
+  provider => 'pip3'
 }
