90e0010014481287f2cee5edadaa03881a4cc695
CarltonNcube <carltonncube@outlook.com>
2023-11-24 11:20:29 -0800

    installing flask from pip3

---
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -0,0 +1,9 @@
+#!/usr/bin/pup
+
+# Declare a package resource
+package { 'flask':
+  # Ensure the package is installed with the specified version
+  ensure   => '2.1.0',
+  # Use pip3 as the provider to install the package
+  provider => 'pip3',
+}
