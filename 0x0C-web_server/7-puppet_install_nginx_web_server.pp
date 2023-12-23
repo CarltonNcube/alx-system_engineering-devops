c8a72e19109e3d30bf780415fc804e6052e4149f
CarltonNcube <carltonncube@outlook.com>
2023-12-23 06:46:33 -0800

    Install Nginx web server (w/ Puppet)

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,50 +1,39 @@
-#!/usr/bin/env bash
-
-# nginx_config.pp
-
 # Install Nginx package
 package { 'nginx':
-  ensure => present,
+  ensure => installed,
 }
 
-# Configure Nginx
+# Configure Nginx server
 file { '/etc/nginx/sites-available/default':
-  ensure => present,
-  source => 'puppet:///modules/nginx_config/nginx_default',
-  notify => Service['nginx'],
+  ensure  => file,
+  content => "
+server {
+    listen 80 default_server;
+    
+    location /redirect_me {
+        rewrite ^ https://www.youtube.com/@nkulumoncube5051;
+    }
+
+    location / {
+        default_type text/html;
+        return 200 'Hello World!';
+    }
+}
+",
+  notify  => Service['nginx'],
 }
 
-file { '/var/www/html/index.html':
-  ensure  => present,
-  content => 'Hello World!',
-}
-
-file { '/etc/nginx/sites-available/redirect_me':
-  ensure => present,
-  source => 'puppet:///modules/nginx_config/nginx_redirect_me',
-  notify => Service['nginx'],
-}
-
-file { '/etc/nginx/sites-enabled/redirect_me':
+# Enable Nginx site by creating a symbolic link
+file { '/etc/nginx/sites-enabled/default':
   ensure => link,
-  target => '/etc/nginx/sites-available/redirect_me',
+  target => '/etc/nginx/sites-available/default',
   notify => Service['nginx'],
 }
 
+# Ensure Nginx service is running and enabled
 service { 'nginx':
-  ensure  => running,
-  enable  => true,
-  require => Package['nginx'],
+  ensure     => running,
+  enable     => true,
+  subscribe => File['/etc/nginx/sites-available/default'],
 }
 
-# nginx_default template
-file { '/etc/nginx/sites-available/default':
-  ensure => present,
-  content => template('nginx_config/nginx_default.erb'),
-}
-
-# nginx_redirect_me template
-file { '/etc/nginx/sites-available/redirect_me':
-  ensure => present,
-  content => template('nginx_config/nginx_redirect_me.erb'),
-}
