a04698ca5a0e537655087556c211efbc15d693d6
CarltonNcube <carltonncube@outlook.com>
2024-01-02 14:23:14 -0800

    Python script to export data in the JSON format

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-Export to JSON
+Export TODO list progress to JSON format.
 
 Usage:
     python3 2-export_to_JSON.py <employee_id>
@@ -13,7 +13,8 @@ import sys
 
 def export_to_json(employee_id):
     """
-    Retrieve and export TODO list progress for a given employee ID in JSON format.
+    Retrieve and export TODO list progress for a given 
+    employee ID in JSON format.
 
     Args:
         employee_id (int): The ID of the employee.
@@ -24,11 +25,13 @@ def export_to_json(employee_id):
     api_url = f"https://jsonplaceholder.typicode.com/users/{employee_id}"
     todos_url = f"https://jsonplaceholder.typicode.com/todos?userId={employee_id}"
 
-    response_employee = requests.get(api_url)
-    response_todos = requests.get(todos_url)
-
-    if response_employee.status_code != 200 or response_todos.status_code != 200:
-        print("Error: Unable to fetch data from the API.")
+    try:
+        response_employee = requests.get(api_url)
+        response_todos = requests.get(todos_url)
+        response_employee.raise_for_status()
+        response_todos.raise_for_status()
+    except requests.RequestException as e:
+        print(f"Error: Unable to fetch data from the API. {e}")
         sys.exit(1)
 
     employee_info = response_employee.json()
@@ -52,9 +55,9 @@ def export_to_json(employee_id):
 
 if __name__ == "__main__":
     if len(sys.argv) != 2 or not sys.argv[1].isdigit():
-        print("Usage: python3 2-export_to_JSON.py <employee_id>")
+        print("Usage: python3 2-export_to_JSON.py <employee_id>
+                (employee_id should be a positive integer)")
         sys.exit(1)
 
     employee_id = int(sys.argv[1])
     export_to_json(employee_id)
-
