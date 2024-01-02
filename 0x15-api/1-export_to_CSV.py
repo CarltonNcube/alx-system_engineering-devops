dbdae3a8b1ec23996ac3711debcfbf88206e1fa1
CarltonNcube <carltonncube@outlook.com>
2024-01-02 13:47:23 -0800

    Python script to export data in the CSV format

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -10,17 +10,7 @@ import requests
 import csv
 import sys
 
-
 def export_to_csv(employee_id):
-    """
-    Retrieve and export TODO list progress for a given employee ID in CSV format.
-
-    Args:
-        employee_id (int): The ID of the employee.
-
-    Returns:
-        None
-    """
     api_url = f"https://jsonplaceholder.typicode.com/users/{employee_id}"
     todos_url = f"https://jsonplaceholder.typicode.com/todos?userId={employee_id}"
 
@@ -31,26 +21,23 @@ def export_to_csv(employee_id):
         print("Error: Unable to fetch data from the API.")
         sys.exit(1)
 
-    employee_info = response_employee.json()
-    todos = response_todos.json()
+    employee_info, todos = response_employee.json(), response_todos.json()
 
     filename = f"{employee_id}.csv"
     with open(filename, mode='w', newline='') as file:
         writer = csv.writer(file, quoting=csv.QUOTE_ALL)
-        writer.writerow(["USER_ID", "USERNAME", "TASK_COMPLETED_STATUS", "TASK_TITLE"])
+        writer.writerow(["USER_ID", "USERNAME", "TASK_COMPLETED_STATUS",
+            "TASK_TITLE"])
 
-        for task in todos:
-            writer.writerow([employee_id, employee_info['username'],
-                             str(task['completed']), task['title']])
+        [writer.writerow([employee_id, employee_info['username'], 
+            str(task['completed']), task['title']]) for task in todos]
 
     print(f"Data exported to {filename}")
-
 
 if __name__ == "__main__":
     if len(sys.argv) != 2 or not sys.argv[1].isdigit():
         print("Usage: python3 1-export_to_CSV.py <employee_id>")
         sys.exit(1)
 
-    employee_id = int(sys.argv[1])
-    export_to_csv(employee_id)
+    export_to_csv(int(sys.argv[1]))
 
