9459d17e65415a0bdddb48a86ee9d8952c70075c
CarltonNcube <carltonncube@outlook.com>
2024-01-02 15:27:59 -0800

    update

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,62 +1,25 @@
 #!/usr/bin/python3
-"""
-Gather data from an API and export to CSV
-
-Usage:
-    python3 1-export_to_CSV.py <employee_id>
-"""
-
+"""Script to export data in the CSV format"""
 import csv
-import requests
+import requests as r
 import sys
 
 
-def export_to_csv(employee_id):
-    # Fetch user information
-    user_url = f'https://jsonplaceholder.typicode.com/users/{employee_id}'
-    user_info = requests.get(user_url).json()
+def export_to_csv(user_id):
+    url = "https://jsonplaceholder.typicode.com/"
+    user_info = r.get(url + "users/{}".format(user_id)).json()
+    username = user_info.get("username")
+    todos = r.get(url + "todos", params={"userId": user_id}).json()
 
-    if 'id' not in user_info:
-        print(f"Error: Unable to fetch user data for ID {employee_id}")
-        sys.exit(1)
+    with open("{}.csv".format(user_id), "w", newline="") as csvfile:
+        writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
+        [writer.writerow([user_id, username, elm.get("completed"),
+            elm.get("title")]) for elm in todos]
 
-    # Fetch user's tasks
-    todos_url = f'https://jsonplaceholder.typicode.com/todos'
-    todos = requests.get(todos_url, params={'userId': employee_id}).json()
-
-    # Prepare data for CSV
-    csv_data = [
-        [user_info['id'], user_info['username'], str(task['completed']),
-         task['title']]
-        for task in todos
-    ]
-
-    # Write to CSV
-    csv_filename = f"{employee_id}.csv"
-    with open(csv_filename, 'w', newline='') as csvfile:
-        csv_writer = csv.writer(csvfile)
-        csv_writer.writerow(["USER_ID", "USERNAME", "TASK_COMPLETED_STATUS",
-                             "TASK_TITLE"])
-        csv_writer.writerows(csv_data)
-
-    print(f"Number of tasks in CSV: OK")
-    print(f"Data exported to {csv_filename}")
-
-    # Output user ID and username
-    print(f"User ID and Username: OK")
-
-    # Check formatting for each task
-    for i, task in enumerate(todos, start=1):
-        if i <= 20:
-            print(f"Task {i} Formatting: OK")
-
-    print("Formatting: OK")
-
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     if len(sys.argv) != 2 or not sys.argv[1].isdigit():
-        print("Usage: python3 1-export_to_CSV.py <employee_id>")
+        print("Usage: python3 script.py <user_id>")
         sys.exit(1)
 
-    employee_id = int(sys.argv[1])
-    export_to_csv(employee_id)
+    user_id = sys.argv[1]
+    export_to_csv(user_id)
