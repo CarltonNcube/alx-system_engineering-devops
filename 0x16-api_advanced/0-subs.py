846953005ea909db69aae972f75c9ab15429e02c
CarltonNcube <carltonncube@outlook.com>
2024-01-16 18:37:48 -0800

    a function that queries the Reddit API and returns the number of subscribers

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,29 +1,22 @@
 #!/usr/bin/python3
-"""Exporting csv files"""
+"""
+Queries the Reddit API and retrieves the total number of subscribers
+for a specified subreddit. If the subreddit is invalid, returns 0.
+"""
+
 import requests
 
 def number_of_subscribers(subreddit):
-    """Read Reddit API and return the number of subscribers"""
-    username = 'Code_Break4560'
-    password = '#Gombedza101'
-    user_pass_dict = {
-        'user': username,
-        'passwd': password,
-        'api_type': 'json'
-    }
-    headers = {
-        'user-agent': 'personal use script by Code_Break4560/1.0'
-    }
-    url = f'https://www.reddit.com/r/{subreddit}/about.json'
-    
-    client = requests.session()
-    client.headers = headers
-    
-    r = client.get(url, allow_redirects=False, auth=(username, password))
-    
-    if r.status_code == 200:
-        return r.json()["data"]["subscribers"]
-    else:
-        return 0
+    """
+    Queries the Reddit API
+    - If not a valid subreddit, return 0.
+    """
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    headers = {"User-Agent": "Custom"}
 
-if __name__ == "__main__":
+    response = requests.get(url, headers=headers)
+    return (
+    response.json().get("data", {}).get("subscribers", 0)
+    if response.status_code == 200
+    else 0
+)
