14823ace4913b3929377bf2d1b40d5acb6ad6be4
CarltonNcube <carltonncube@outlook.com>
2024-01-16 18:42:31 -0800

    a function that queries the Reddit API and returns the number of subscribers

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -6,17 +6,18 @@ for a specified subreddit. If the subreddit is invalid, returns 0.
 
 import requests
 
+
 def number_of_subscribers(subreddit):
     """
-    Queries the Reddit API
+    Function that queries the Reddit API
     - If not a valid subreddit, return 0.
     """
-    url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    headers = {"User-Agent": "Custom"}
+    req = requests.get(
+        "https://www.reddit.com/r/{}/about.json".format(subreddit),
+        headers={"User-Agent": "Custom"},
+    )
 
-    response = requests.get(url, headers=headers)
-    return (
-    response.json().get("data", {}).get("subscribers", 0)
-    if response.status_code == 200
-    else 0
-)
+    if req.status_code == 200:
+        return req.json().get("data").get("subscribers")
+    else:
+        return 0
