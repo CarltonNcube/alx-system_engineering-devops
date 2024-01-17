56dc66bb5de05c4209ec9d6ad898443325b28a04
CarltonNcube <carltonncube@outlook.com>
2024-01-16 18:22:42 -0800

    a function that queries the Reddit API and returns the number of subscribers

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,43 +1,29 @@
 #!/usr/bin/python3
-
-"""
-Module Name: reddit_api
-Description: This module interacts with the Reddit API to get the number of subscribers for a given subreddit.
-"""
-
+"""Exporting csv files"""
 import requests
 
 def number_of_subscribers(subreddit):
-    """
-    Get the number of subscribers for a given subreddit.
-
-    Parameters:
-    - subreddit (str): The name of the subreddit.
-
-    Returns:
-    - int: Number of subscribers if the subreddit is valid, 0 otherwise.
-    """
-    # Reddit API endpoint for subreddit information
-    url = f"https://www.reddit.com/r/{subreddit}/about.json"
-
-    # Set a custom User-Agent to avoid Too Many Requests error
-    headers = {'User-Agent': 'CustomUserAgent'}
-
-    # Send a GET request to the Reddit API
-    response = requests.get(url, headers=headers)
-
-    # Check if the request was successful (status code 200)
-    if response.status_code == 200:
-        # Parse the JSON response
-        subreddit_info = response.json()
-
-        # Extract and return the number of subscribers
-        return subreddit_info['data']['subscribers']
-    elif response.status_code == 302:
-        # This may happen if the subreddit is invalid (redirect to search results)
-        return 0
+    """Read Reddit API and return the number of subscribers"""
+    username = 'Code_Break4560'
+    password = '#Gombedza101'
+    user_pass_dict = {
+        'user': username,
+        'passwd': password,
+        'api_type': 'json'
+    }
+    headers = {
+        'user-agent': 'personal use script by Code_Break4560/1.0'
+    }
+    url = f'https://www.reddit.com/r/{subreddit}/about.json'
+    
+    client = requests.session()
+    client.headers = headers
+    
+    r = client.get(url, allow_redirects=False, auth=(username, password))
+    
+    if r.status_code == 200:
+        return r.json()["data"]["subscribers"]
     else:
-        # Other errors, return 0
         return 0
 
 if __name__ == "__main__":
