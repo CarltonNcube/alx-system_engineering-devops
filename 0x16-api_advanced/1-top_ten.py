1503fc00ac986fc22cca1f8bdee037e14467723c
CarltonNcube <carltonncube@outlook.com>
2024-01-16 19:07:36 -0800

    a function that queries the Reddit API and prints the titles of the first 10 hot posts listed for a given subreddit

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,39 +1,23 @@
 #!/usr/bin/python3
-
+"""
+1-top_ten
+"""
 import requests
 
 def top_ten(subreddit):
     """
-    Print the titles of the first 10 hot posts for a given subreddit.
-
-    Parameters:
-    - subreddit (str): The name of the subreddit.
-
-    Returns:
-    - None
+    Function that queries the Reddit API
+    and prints the titles of the first 10 hot posts
+    for a given subreddit.
     """
-    # Reddit API endpoint for hot posts in a subreddit
-    url = f"https://www.reddit.com/r/{subreddit}/hot.json"
-
-    # Set a custom User-Agent to avoid Too Many Requests error
-    headers = {'User-Agent': 'CustomUserAgent'}
-
-    # Send a GET request to the Reddit API
+    url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
+    headers = {"User-Agent": "Custom"}
+    
     response = requests.get(url, headers=headers)
 
-    # Check if the request was successful (status code 200)
     if response.status_code == 200:
-        # Parse the JSON response
-        posts_data = response.json()['data']['children']
-
-        # Print titles of the first 10 posts
-        for i, post in enumerate(posts_data[:10], 1):
-            print(f"{i}. {post['data']['title']}")
-    elif response.status_code == 302:
-        # This may happen if the subreddit is invalid (redirect to search results)
-        print("None")
+        data = response.json().get("data", {}).get("children", [])
+        for post in data:
+            print(post.get("data", {}).get("title"))
     else:
-        # Other errors, print None
-        print("None")
-
-if __name__ == "__main__":
+        print(None)
