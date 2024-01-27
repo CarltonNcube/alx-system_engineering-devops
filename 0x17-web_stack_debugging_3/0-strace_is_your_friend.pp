6915172134c12a153068a5261c0c6745e18fc2bf
CarltonNcube <carltonncube@outlook.com>
2024-01-27 13:38:13 -0800

    Script to fix Apache is returning a 500 error

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,6 @@
 # Puppet manifest to fix PHP extension issue
 
-exec { 'fix php ext':
+exec { 'fix-wordpress':
     command => 'sed -i "s/php/phpp/g" /var/www/html/wp-settings.php',
     path    => '/usr/local/bin/:/bin/'
 }
